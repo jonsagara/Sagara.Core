3a90f68811deb7f85e3e1093efce83f8218e983a
Jon Sagara <jonsagara@gmail.com>
2023-08-21 14:43:35 -0700

    Fixed the resx file inclusion in the .csproj.

--- src/Sagara.Core/Validation/ValidationHelperResources.Designer.cs
+++ src/Sagara.Core/Validation/ValidationHelperResources.Designer.cs
@@ -39,7 +39,7 @@ namespace Sagara.Core.Validation {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Sagara.Common.Validation.ValidationHelperResources", typeof(ValidationHelperResources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Sagara.Core.Validation.ValidationHelperResources", typeof(ValidationHelperResources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
