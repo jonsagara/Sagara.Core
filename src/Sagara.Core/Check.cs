7399ec66e06acaa8dfd81930efbc28c96a10cd6a
Jon Sagara <jonsagara@gmail.com>
2023-08-21 10:51:47 -0700

    Initial commit.

---
+++ src/Sagara.Core/Check.cs
@@ -0,0 +1,165 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Sagara.Core;
+
+/// <summary>
+/// Argument checker so that we don't have to keep writing the same guard clauses all over the place.
+/// </summary>
+/// <remarks>
+/// This updated version uses the new <see cref="CallerArgumentExpressionAttribute"/> to get the 
+/// argument expression from the call site so that we don't have to manually pass that in.
+/// </remarks>
+public static class Check
+{
+    /// <summary>
+    /// Throws an <see cref="ArgumentNullException" /> if the value is null.
+    /// </summary>
+    public static void NotNull<T>([NotNull] T value, [CallerArgumentExpression("value")] string? valueArgExpression = null,
+        [CallerMemberName] string? memberName = null, [CallerLineNumber] int sourceLineNumber = 0, [CallerFilePath] string? sourceFilePath = null)
+    {
+        if (value is null)
+        {
+            NotEmpty(valueArgExpression, nameof(valueArgExpression), memberName, sourceLineNumber, sourceFilePath);
+
+            throw new ArgumentNullException(valueArgExpression, FormatCallerInfo(valueArgExpression, memberName, sourceLineNumber, sourceFilePath));
+        }
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentNullException"/> if the string is null, or an <see cref="ArgumentException"/> if the
+    /// string is null or white space.
+    /// </summary>
+    public static void NotEmpty([NotNull] string? value, [CallerArgumentExpression("value")] string? valueArgExpression = null,
+        [CallerMemberName] string? memberName = null, [CallerLineNumber] int sourceLineNumber = 0, [CallerFilePath] string? sourceFilePath = null)
+    {
+        Exception? ex = null;
+
+        if (value is null)
+        {
+            ex = new ArgumentNullException(valueArgExpression, FormatCallerInfo(valueArgExpression, memberName, sourceLineNumber, sourceFilePath));
+        }
+        else if (value.Trim().Length == 0)
+        {
+            ex = new ArgumentException(
+                message: $"{valueArgExpression} is empty or white space. Caller: {FormatCallerInfo(valueArgExpression, memberName, sourceLineNumber, sourceFilePath)}",
+                paramName: valueArgExpression
+                );
+        }
+
+        if (ex is not null)
+        {
+            NotEmpty(valueArgExpression, nameof(valueArgExpression), memberName, sourceLineNumber, sourceFilePath);
+
+            throw ex;
+        }
+
+        // Justification: value will be non-null when exiting. If it is null, this method throws.
+#pragma warning disable CS8777 // Parameter must have a non-null value when exiting.
+    }
+#pragma warning restore CS8777 // Parameter must have a non-null value when exiting.
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentException"/> if the Guid equals Guid.Empty.
+    /// </summary>
+    public static void NotEmpty(Guid value, [CallerArgumentExpression("value")] string? valueArgExpression = null,
+        [CallerMemberName] string? memberName = null, [CallerLineNumber] int sourceLineNumber = 0, [CallerFilePath] string? sourceFilePath = null)
+    {
+        if (value == Guid.Empty)
+        {
+            throw new ArgumentException(message:
+                $"{valueArgExpression} equals Guid.Empty. Caller: {FormatCallerInfo(valueArgExpression, memberName, sourceLineNumber, sourceFilePath)}",
+                paramName: valueArgExpression
+                );
+        }
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentNullException"/> if the collection is null, or an <see cref="ArgumentException"/> if
+    /// the collection is empty.
+    /// </summary>
+    public static void NotEmpty<T>([NotNull] IReadOnlyCollection<T> value, [CallerArgumentExpression("value")] string? valueArgExpression = null,
+        [CallerMemberName] string? memberName = null, [CallerLineNumber] int sourceLineNumber = 0, [CallerFilePath] string? sourceFilePath = null)
+    {
+        NotNull(value, valueArgExpression, memberName, sourceLineNumber, sourceFilePath);
+
+        if (value.Count == 0)
+        {
+            NotEmpty(valueArgExpression, nameof(valueArgExpression), memberName, sourceLineNumber, sourceFilePath);
+
+            throw new ArgumentException(
+                message: $"Collection argument is empty. Caller: {FormatCallerInfo(valueArgExpression, memberName, sourceLineNumber, sourceFilePath)}",
+                paramName: valueArgExpression
+                );
+        }
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentNullException"/> if the collection is null, or an <see cref="ArgumentException"/> if
+    /// the collection has one or more null values.
+    /// </summary>
+    public static void HasNoNulls<T>([NotNull] IReadOnlyList<T> value, [CallerArgumentExpression("value")] string? valueArgExpression = null,
+        [CallerMemberName] string? memberName = null, [CallerLineNumber] int sourceLineNumber = 0, [CallerFilePath] string? sourceFilePath = null)
+    {
+        NotNull(value, valueArgExpression, memberName, sourceLineNumber, sourceFilePath);
+
+        if (value.Any(v => v is null))
+        {
+            NotEmpty(valueArgExpression, nameof(valueArgExpression), memberName, sourceLineNumber, sourceFilePath);
+
+            throw new ArgumentException(
+                message: $"Collection has one or more null values. Caller: {FormatCallerInfo(valueArgExpression, memberName, sourceLineNumber, sourceFilePath)}",
+                paramName: valueArgExpression
+                );
+        }
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentOutOfRangeException"/> if <paramref name="value"/> is outside the range of 
+    /// [<paramref name="rangeLo"/>, <paramref name="rangeHi"/>].
+    /// </summary>
+    public static void NotOutOfRange<T>(T value, T rangeLo, T rangeHi, [CallerArgumentExpression("value")] string? valueArgExpression = null,
+        [CallerMemberName] string? memberName = null, [CallerLineNumber] int sourceLineNumber = 0, [CallerFilePath] string? sourceFilePath = null)
+        where T : IComparable, IComparable<T>
+    {
+        if (rangeLo.CompareTo(rangeHi) > 0)
+        {
+            throw new ArgumentException($"{nameof(rangeLo)} must be less than or equal to {nameof(rangeHi)}. Caller: {FormatCallerInfo(valueArgExpression, memberName, sourceLineNumber, sourceFilePath)}", nameof(rangeLo));
+        }
+
+        if (value.CompareTo(rangeLo) < 0 || value.CompareTo(rangeHi) > 0)
+        {
+            throw new ArgumentOutOfRangeException(valueArgExpression, value, $"{valueArgExpression} was outside the range of [{rangeLo}, {rangeHi}].  Caller: {FormatCallerInfo(valueArgExpression, memberName, sourceLineNumber, sourceFilePath)}");
+        }
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentNullException"/> if the collection is null, or an <see cref="ArgumentException"/> if
+    /// the collection has one or more null or white space strings.
+    /// </summary>
+    public static void HasNoEmpties([NotNull] IReadOnlyCollection<string> value, [CallerArgumentExpression("value")] string? valueArgExpression = null,
+        [CallerMemberName] string? memberName = null, [CallerLineNumber] int sourceLineNumber = 0, [CallerFilePath] string? sourceFilePath = null)
+    {
+        NotNull(value, valueArgExpression, memberName, sourceLineNumber, sourceFilePath);
+
+        if (value.Any(v => string.IsNullOrWhiteSpace(v)))
+        {
+            NotEmpty(valueArgExpression, nameof(valueArgExpression), memberName, sourceLineNumber, sourceFilePath);
+
+            throw new ArgumentException(
+                message: $"Collection of strings has one or more null or white space values. Caller: {FormatCallerInfo(valueArgExpression, memberName, sourceLineNumber, sourceFilePath)}",
+                paramName: valueArgExpression
+                );
+        }
+    }
+
+
+    //
+    // Private methods
+    //
+
+    private static string FormatCallerInfo(string? valueArgExpression, string? memberName, int sourceLineNumber, string? sourceFilePath)
+    {
+        return $"Failing argument expression '{valueArgExpression}' called by {memberName} at Line {sourceLineNumber} in '{sourceFilePath}'.";
+    }
+}
---
+++ src/Sagara.Core/SequentialGuid.cs
@@ -0,0 +1,114 @@
+﻿using System.Buffers.Binary;
+
+namespace Sagara.Core;
+
+/// <summary>
+/// Generates Guids sequentially.
+/// </summary>
+public class SequentialGuid
+{
+    /// <summary>
+    /// Equivalent to 'SELECT CAST(0 AS DATETIME)'. Specify UTC since we're using UTC in GenerateComb, and 
+    /// since all the servers we run on are set to UTC.
+    /// </summary>
+    private static readonly long _epochTicks = new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Utc).Ticks;
+
+    private static readonly int[] _sqlOrderMap = new int[16] { 3, 2, 1, 0, 5, 4, 7, 6, 9, 8, 15, 14, 13, 12, 11, 10 };
+
+    /// <summary>
+    /// Generate a sequential Guid.
+    /// </summary>
+    /// <returns></returns>
+    public static Guid GenerateComb()
+    {
+        //
+        // NOTE: the last 8 bytes of a UNIQUEIDENTIFIER in SQL Server are stored as Big Endian. Since we're
+        //   overwriting the last 6 bytes, we need to convert those numbers to Big Endian.
+        //
+        //  Bits    Bytes   Name    Endianness
+        //  32      4       Data1   Native
+        //  16      2       Data2   Native
+        //  16      2       Data3   Native
+        //  64      8       Data4   Big
+        //
+        // Native on Windows means Little Endian.
+        //
+        // See: https://dba.stackexchange.com/a/121878
+        //
+
+        // Generate a new Guid. We'll alter its last 6 bytes to make it less random and more sequential.
+        Span<byte> guidBytes = stackalloc byte[16];
+        Guid.NewGuid().TryWriteBytes(guidBytes);
+
+        var now = DateTime.UtcNow;
+
+        // Get the days elapsed since the SQL Server DATETIME epoch.
+        var timeSinceEpoch = new TimeSpan(now.Ticks - _epochTicks);
+        Span<byte> daysBytes = stackalloc byte[4];
+        BinaryPrimitives.WriteInt32BigEndian(daysBytes, timeSinceEpoch.Days);
+
+        // Get the current time of day.
+        // NOTE: SQL Server is accurate to 1/300th of a millisecond, so we divide by 3.333333 
+        Span<byte> msecsBytes = stackalloc byte[8];
+        BinaryPrimitives.WriteInt64BigEndian(msecsBytes, (long)(now.TimeOfDay.TotalMilliseconds / 3.333333));
+
+        // Copy the days and milliseconds bytes into the end of the Guid, starting at offset 10.
+        daysBytes.Slice(start: daysBytes.Length - 2, length: 2).CopyTo(guidBytes.Slice(start: guidBytes.Length - 6));
+        msecsBytes.Slice(start: msecsBytes.Length - 4, length: 4).CopyTo(guidBytes.Slice(start: guidBytes.Length - 4));
+
+        return new Guid(guidBytes);
+    }
+
+    /// <summary>
+    /// The current sequential Guid value.
+    /// </summary>
+    public Guid CurrentGuid { get; private set; }
+
+    /// <summary>
+    /// Default .ctor. Initializes <see cref="CurrentGuid"/> with a new value.
+    /// </summary>
+    public SequentialGuid()
+    {
+        CurrentGuid = GenerateComb();
+    }
+
+    /// <summary>
+    /// Constructor that initializes <see cref="CurrentGuid"/> with the provided Guid.
+    /// </summary>
+    /// <param name="previousGuid">The guid to use to initialize this instance.</param>
+    public SequentialGuid(Guid previousGuid)
+    {
+        CurrentGuid = previousGuid;
+    }
+
+    /// <summary>
+    /// Increment one or more bytes of <see cref="CurrentGuid"/> to get the next sequential Guid,
+    /// and then copy this new value back to <see cref="CurrentGuid"/>.
+    /// </summary>
+    /// <param name="sequentialGuid">The current sequential Guid.</param>
+    /// <returns>The incremented sequential Guid.</returns>
+    public static SequentialGuid operator ++(SequentialGuid sequentialGuid)
+    {
+        // Copy the current Guid's value into a byte array.
+        Span<byte> currentGuidBytes = stackalloc byte[16];
+        sequentialGuid.CurrentGuid.TryWriteBytes(currentGuidBytes);
+
+        for (var ixSqlOrderMap = 0; ixSqlOrderMap < 16; ixSqlOrderMap++)
+        {
+            // Get the byte to increment, and increment it.
+            var ixByteToIncrement = _sqlOrderMap[ixSqlOrderMap];
+            currentGuidBytes[ixByteToIncrement]++;
+
+            // If the byte we just modified is not 0, exit. We don't need to increment
+            //   more significant bytes.
+            if (currentGuidBytes[ixByteToIncrement] != 0)
+            {
+                break;
+            }
+        }
+
+        // Update the current Guid with the newly-incremented Guid.
+        sequentialGuid.CurrentGuid = new Guid(currentGuidBytes);
+        return sequentialGuid;
+    }
+}
