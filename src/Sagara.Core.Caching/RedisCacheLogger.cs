5f9d9690eaa86d5b6f9eae0c2b06fa6cde4a3820
Jon Sagara <jonsagara@gmail.com>
2023-12-21 19:36:27 -0800

    Remove the EventId parameter from LoggerMessage because it's always 0.

--- src/Sagara.Core.Caching/RedisCacheLogger.cs
+++ src/Sagara.Core.Caching/RedisCacheLogger.cs
@@ -9,24 +9,24 @@ namespace Sagara.Core.Caching;
 /// </summary>
 internal static partial class RedisCacheLogger
 {
-    [LoggerMessage(EventId = 0, Level = LogLevel.Error, Message = "{LogPrefix}ConnectionFailed: Connection type '{ConnectionType}' on EndPoint '{EndPoint}' reported ConnectionFailureType '{FailureType}'")]
+    [LoggerMessage(Level = LogLevel.Error, Message = "{LogPrefix}ConnectionFailed: Connection type '{ConnectionType}' on EndPoint '{EndPoint}' reported ConnectionFailureType '{FailureType}'")]
     public static partial void OnConnectionFailed(this ILogger logger, Exception? ex, string logPrefix, ConnectionType connectionType, EndPoint? endPoint, ConnectionFailureType failureType);
 
-    [LoggerMessage(EventId = 0, Level = LogLevel.Information, Message = "{LogPrefix}ConnectionRestored: Connection type '{ConnectionType}' on EndPoint '{EndPoint}' reported ConnectionFailureType '{FailureType}'")]
+    [LoggerMessage(Level = LogLevel.Information, Message = "{LogPrefix}ConnectionRestored: Connection type '{ConnectionType}' on EndPoint '{EndPoint}' reported ConnectionFailureType '{FailureType}'")]
     public static partial void OnConnectionRestored(this ILogger logger, Exception? ex, string logPrefix, ConnectionType connectionType, EndPoint? endPoint, ConnectionFailureType failureType);
 
-    [LoggerMessage(EventId = 0, Level = LogLevel.Error, Message = "{LogPrefix}ErrorMessage: Server '{EndPoint}' reported this error message: {Message}")]
+    [LoggerMessage(Level = LogLevel.Error, Message = "{LogPrefix}ErrorMessage: Server '{EndPoint}' reported this error message: {Message}")]
     public static partial void OnErrorMessage(this ILogger logger, string logPrefix, EndPoint? endPoint, string message);
 
-    [LoggerMessage(EventId = 0, Level = LogLevel.Warning, Message = "{LogPrefix}ServerMaintenanceEvent: Server maintenance event received at {ReceivedTimeUtc}. Expected start time is {StartTimeUtc}. Raw message: {RawMessage}")]
+    [LoggerMessage(Level = LogLevel.Warning, Message = "{LogPrefix}ServerMaintenanceEvent: Server maintenance event received at {ReceivedTimeUtc}. Expected start time is {StartTimeUtc}. Raw message: {RawMessage}")]
     public static partial void OnServerMaintenanceEvent(this ILogger logger, string logPrefix, DateTime receivedTimeUtc, DateTime? startTimeUtc, string? rawMessage);
 
-    [LoggerMessage(EventId = 0, Level = LogLevel.Error, Message = "Unhandled exception trying to async {Command} {Key}")]
+    [LoggerMessage(Level = LogLevel.Error, Message = "Unhandled exception trying to async {Command} {Key}")]
     public static partial void UnhandledException(this ILogger logger, Exception ex, string command, string key);
 
-    [LoggerMessage(EventId = 0, Level = LogLevel.Error, Message = "Unhandled exception trying to SUBSCRIBE {Channel}")]
+    [LoggerMessage(Level = LogLevel.Error, Message = "Unhandled exception trying to SUBSCRIBE {Channel}")]
     public static partial void UnhandledSubscribeException(this ILogger logger, Exception ex, RedisChannel channel);
 
-    [LoggerMessage(EventId = 0, Level = LogLevel.Error, Message = "Unhandled exception trying to async PUBLISH {Channel} {Message}")]
+    [LoggerMessage(Level = LogLevel.Error, Message = "Unhandled exception trying to async PUBLISH {Channel} {Message}")]
     public static partial void UnhandledPublishException(this ILogger logger, Exception ex, RedisChannel channel, RedisValue message);
 }
