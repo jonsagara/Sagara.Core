2761897de35fb308a36d369a75a99dd1b8ef78d6
Jon Sagara <jonsagara@gmail.com>
2025-03-08 15:45:50 -0800

    Added NumberExtensions. Upgraded SE.Redis to the latest version to get bug fixes.

---
+++ src/Sagara.Core/Extensions/NumberExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+
+namespace Sagara.Core.Extensions;
+
+public static class NumberExtensions
+{
+    /// <summary>
+    /// If <paramref name="value"/> is <c>TNumber.Zero</c>, return <c>null</c>. Otherwise, return
+    /// <paramref name="value"/> unchanged.
+    /// </summary>
+    public static TNumber? ToNullIfZero<TNumber>(this TNumber value)
+        where TNumber : struct, INumber<TNumber>
+        => value == TNumber.Zero ? null : value;
+}
