3031d7c887dab76a0f22ec786fbac91567f839a7
Jon Sagara <jonsagara@gmail.com>
2025-03-24 16:04:22 -0700

    TryGetValue is faster than GetOrAdd, but still uses the same amount of memory.

--- src/Sagara.Core.Benchmarks/Harnesses/ResourceFormatStringCaching/FormatStringHarness.cs
+++ src/Sagara.Core.Benchmarks/Harnesses/ResourceFormatStringCaching/FormatStringHarness.cs
@@ -13,14 +13,14 @@ namespace Sagara.Core.Benchmarks.Harnesses.ResourceFormatStringCaching;
 public class FormatStringHarness
 {
 
-/*
- * Findings:
- * 
- * The cached version that uses struct keys is faster by about 50 ns, but uses about 50 bytes more of memory per
- * invocation. I don't think the speed savings warrant the increased memory use.
- * 
- * Don't even consider the version that uses a record as the cache key: it's faster, but uses way more memory per
- * invocation.
+    /*
+     * Findings:
+     * 
+     * The cached version that uses struct keys is faster by about 50 ns, but uses about 50 bytes more of memory per
+     * invocation. I don't think the speed savings warrant the increased memory use.
+     * 
+     * Don't even consider the version that uses a record as the cache key: it's faster, but uses way more memory per
+     * invocation.
 
 // * Summary *
 
@@ -32,13 +32,14 @@ Intel Core i7-1065G7 CPU 1.30GHz, 1 CPU, 8 logical and 4 physical cores
 
 Job=.NET 9.0  Runtime=.NET 9.0
 
-| Method                                    | Mean     | Error   | StdDev  | Ratio | RatioSD | Gen0   | Allocated | Alloc Ratio |
-|------------------------------------------ |---------:|--------:|--------:|------:|--------:|-------:|----------:|------------:|
-| FormatRawResourceString                   | 364.2 ns | 4.65 ns | 4.35 ns |  1.00 |    0.02 | 0.0534 |     224 B |        1.00 |
-| FormatCachedParsedResourceString          | 330.8 ns | 4.94 ns | 4.62 ns |  0.91 |    0.02 | 0.0744 |     312 B |        1.39 |
-| FormatCachedParsedResourceStringStructKey | 317.2 ns | 6.16 ns | 6.85 ns |  0.87 |    0.02 | 0.0648 |     272 B |        1.21 |
+| Method                                               | Mean     | Error   | StdDev  | Ratio | RatioSD | Gen0   | Allocated | Alloc Ratio |
+|----------------------------------------------------- |---------:|--------:|--------:|------:|--------:|-------:|----------:|------------:|
+| FormatRawResourceString                              | 367.4 ns | 5.20 ns | 4.87 ns |  1.00 |    0.02 | 0.0534 |     224 B |        1.00 |
+| FormatCachedParsedResourceString                     | 337.1 ns | 5.41 ns | 5.06 ns |  0.92 |    0.02 | 0.0744 |     312 B |        1.39 |
+| FormatCachedParsedResourceStringStructKey            | 316.1 ns | 6.16 ns | 7.33 ns |  0.86 |    0.02 | 0.0648 |     272 B |        1.21 |
+| FormatCachedParsedResourceStringStructKeyTryGetValue | 309.1 ns | 3.72 ns | 3.48 ns |  0.84 |    0.01 | 0.0648 |     272 B |        1.21 |
 
-*/
+    */
 
     private const string _propertyName = "Cost";
     private const double _value = 0.99;
@@ -60,6 +61,12 @@ Job=.NET 9.0  Runtime=.NET 9.0
     public string FormatCachedParsedResourceStringStructKey()
     {
         return ValidationHelperResources.ResourceManager.FormatStructKey(resourceKey: nameof(ValidationHelperResources.GreaterThanOrEqual), _propertyName, _threshold, _value);
+    }
+
+    [Benchmark]
+    public string FormatCachedParsedResourceStringStructKeyTryGetValue()
+    {
+        return ValidationHelperResources.ResourceManager.FormatStructKeyTryGetValue(resourceKey: nameof(ValidationHelperResources.GreaterThanOrEqual), _propertyName, _threshold, _value);
     }
 }
 
@@ -124,6 +131,36 @@ public static class ResourceFormatStrings
             },
             factoryArgument: resourceManager
             );
+
+        return string.Format(CultureInfo.CurrentCulture, compositeFormat, args);
+    }
+
+    public static string FormatStructKeyTryGetValue(this ResourceManager resourceManager, string resourceKey, params object[] args)
+    {
+        ArgumentNullException.ThrowIfNull(resourceManager);
+        ArgumentException.ThrowIfNullOrWhiteSpace(resourceKey);
+        ArgumentNullException.ThrowIfNull(args);
+
+        var cacheKey = new CompositeFormatCacheKeyStruct(
+            ResourceKey: resourceKey,
+            CurrentCultureName: CultureInfo.CurrentCulture.Name,
+            CurrentUICultureName: CultureInfo.CurrentUICulture.Name
+            );
+
+
+        CompositeFormat? compositeFormat = null;
+
+        if (!_localizedFormatStringsStructKey.TryGetValue(key: cacheKey, out compositeFormat))
+        {
+            var localizedFormatString = resourceManager.GetString(resourceKey, CultureInfo.CurrentUICulture);
+            if (string.IsNullOrWhiteSpace(localizedFormatString))
+            {
+                throw new InvalidOperationException($"Resource '{cacheKey.ResourceKey}' not found in ResourceManager '{resourceManager.BaseName}' for CurrentCulture '{cacheKey.CurrentCultureName}' and CurrentUICulture '{cacheKey.CurrentUICultureName}'.");
+            }
+
+            compositeFormat = CompositeFormat.Parse(localizedFormatString);
+            _localizedFormatStringsStructKey.TryAdd(cacheKey, compositeFormat);
+        }
 
         return string.Format(CultureInfo.CurrentCulture, compositeFormat, args);
     }
