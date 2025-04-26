70cd1fb592dd2878d11fbef951764d11d227d190
Jon Sagara <jonsagara@gmail.com>
2025-04-26 08:58:56 -0700

    Added Left and Right string extensions.

---
+++ src/Sagara.Core.Tests/ExtensionTests/StringExtensionTests.cs
@@ -0,0 +1,146 @@
+﻿using Sagara.Core.Extensions;
+using Xunit;
+
+namespace Sagara.Core.Tests.ExtensionTests;
+
+public class StringExtensionTests
+{
+    //
+    // Left
+    //
+
+    [Theory]
+    [InlineData("Hello")]
+    [InlineData("a")]
+    [InlineData("Hello, World! This is amazing!")]
+    public void Left_LengthEquals_ReturnsWholeString(string value)
+    {
+        Assert.Equal(expected: value, actual: value.Left(value.Length));
+    }
+
+    [Theory]
+    [InlineData("Hello", 30)]
+    [InlineData("a", 52)]
+    [InlineData("Hello, World! This is amazing!", int.MaxValue)]
+    public void Left_LengthGreaterThanValue_ReturnsWholeString(string value, int length)
+    {
+        Assert.Equal(expected: value, actual: value.Left(length));
+    }
+
+    [Theory]
+    [InlineData("Hello")]
+    [InlineData("a")]
+    [InlineData("Hello, World! This is amazing!")]
+    public void Left_Length1_ReturnsFirstCharacter(string value)
+    {
+        Assert.Equal(expected: $"{value[0]}", actual: value.Left(1));
+    }
+
+    [Theory]
+    [InlineData("Hello", 3)]
+    [InlineData("a", 1)]
+    [InlineData("Hello, World! This is amazing!", 10)]
+    public void Left_LengthN_ReturnsFirstNCharacters(string value, int length)
+    {
+        Assert.Equal(expected: value[..length], actual: value.Left(length));
+    }
+
+    [Theory]
+    [InlineData("Hello")]
+    [InlineData("a")]
+    [InlineData("Hello, World! This is amazing!")]
+    public void Left_Length0_ReturnsEmptyString(string value)
+    {
+        Assert.Equal(expected: "", actual: value.Left(0));
+    }
+
+    [Theory]
+    [InlineData("Hello", -1)]
+    [InlineData("a", -2)]
+    [InlineData("Hello, World! This is amazing!", int.MinValue)]
+    public void Left_LengthLessThan0_Throws(string value, int length)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => value.Left(length));
+    }
+
+    [Fact]
+    public void Left_NullValue_Throws()
+    {
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+        // Justification: testing that null throws.
+        Assert.Throws<ArgumentNullException>(() => 
+            ((string?)null).Left(12)
+            );
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+    }
+
+
+    //
+    // Right
+    //
+
+    [Theory]
+    [InlineData("Hello")]
+    [InlineData("a")]
+    [InlineData("Hello, World! This is amazing!")]
+    public void Right_LengthEquals_ReturnsWholeString(string value)
+    {
+        Assert.Equal(expected: value, actual: value.Right(value.Length));
+    }
+
+    [Theory]
+    [InlineData("Hello", 30)]
+    [InlineData("a", 52)]
+    [InlineData("Hello, World! This is amazing!", int.MaxValue)]
+    public void Right_LengthGreaterThanValue_ReturnsWholeString(string value, int length)
+    {
+        Assert.Equal(expected: value, actual: value.Right(length));
+    }
+
+    [Theory]
+    [InlineData("Hello")]
+    [InlineData("a")]
+    [InlineData("Hello, World! This is amazing!")]
+    public void Right_Length1_ReturnsLastCharacter(string value)
+    {
+        Assert.Equal(expected: $"{value[^1]}", actual: value.Right(1));
+    }
+
+    [Theory]
+    [InlineData("Hello", 3)]
+    [InlineData("a", 1)]
+    [InlineData("Hello, World! This is amazing!", 10)]
+    public void Right_LengthN_ReturnsLastNCharacters(string value, int length)
+    {
+        Assert.Equal(expected: value[^length..], actual: value.Right(length));
+    }
+
+    [Theory]
+    [InlineData("Hello")]
+    [InlineData("a")]
+    [InlineData("Hello, World! This is amazing!")]
+    public void Right_Length0_ReturnsEmptyString(string value)
+    {
+        Assert.Equal(expected: "", actual: value.Right(0));
+    }
+
+    [Theory]
+    [InlineData("Hello", -1)]
+    [InlineData("a", -2)]
+    [InlineData("Hello, World! This is amazing!", int.MinValue)]
+    public void Right_LengthLessThan0_Throws(string value, int length)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => value.Right(length));
+    }
+
+    [Fact]
+    public void Right_NullValue_Throws()
+    {
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+        // Justification: testing that null throws.
+        Assert.Throws<ArgumentNullException>(() =>
+            ((string?)null).Right(12)
+            );
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+    }
+}
---
+++ src/Sagara.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,42 @@
+﻿namespace Sagara.Core.Extensions;
+
+public static class StringExtensions
+{
+    /// <summary>
+    /// Take the left-most <paramref name="length"/> characters of the given string.
+    /// </summary>
+    /// <param name="value">The string value.</param>
+    /// <param name="length">The maximum string length of the returned string.</param>
+    /// <returns>
+    /// <para>If <paramref name="value"/>.Length &gt;= <paramref name="length"/>, return the left-most <paramref name="length"/> characters.</para>
+    /// <para>If <paramref name="value"/>.Length &lt; length, return <paramref name="value"/> unchanged.</para>
+    /// </returns>
+    public static string Left(this string value, int length)
+    {
+        Check.NotNull(value);
+        ArgumentOutOfRangeException.ThrowIfLessThan(length, 0);
+
+        return value.Length >= length
+            ? value.Substring(startIndex: 0, length: length)
+            : value;
+    }
+
+    /// <summary>
+    /// Take the right-most <paramref name="length"/> characters of the given string.
+    /// </summary>
+    /// <param name="value">The string value.</param>
+    /// <param name="length">The maximum string length of the returned string.</param>
+    /// <returns>
+    /// <para>If <paramref name="value"/>.Length &gt;= <paramref name="length"/>, return the right-most <paramref name="length"/> characters.</para>
+    /// <para>If <paramref name="value"/>.Length &lt; length, return <paramref name="value"/> unchanged.</para>
+    /// </returns>
+    public static string Right(this string value, int length)
+    {
+        Check.NotNull(value);
+        ArgumentOutOfRangeException.ThrowIfLessThan(length, 0);
+
+        return value.Length >= length
+            ? value.Substring(startIndex: value.Length - length, length: length)
+            : value;
+    }
+}
