bd678b403a5f6d0aeba7bc7f84bd473c7a8c828c
Jon Sagara <jonsagara@gmail.com>
2025-12-11 13:56:25 -0800

    Put RandomString methods in roughly alphabetical order.

--- src/Sagara.Core/RandomString.cs
+++ src/Sagara.Core/RandomString.cs
@@ -44,29 +44,49 @@ public static class RandomString
 
 
     /// <summary>
-    /// Generates a cryptographically-strong array of random bytes and return them as a base64 url-encoded string. 
+    /// Generate a cryptographically-strong array of random bytes and return them encoded as a string that
+    /// can contain the characters in [a-zA-Z0-9-_], as well as the various symbol characters.
     /// </summary>
-    /// <remarks>The underlying <see cref="Base64Url.EncodeToString(ReadOnlySpan{byte})"/> omits the optional padding characters.</remarks>
-    /// <param name="byteCount">The number of random bytes to generate.</param>
-    /// <returns>The random bytes as a base64 url-encoded string.</returns>
-    // Justification: It's a base64 URL-encoded string, not a URI.
-#pragma warning disable CA1055 // URI-like return values should not be strings
-    public static string GenerateBase64UrlEncoded(int byteCount)
-#pragma warning restore CA1055 // URI-like return values should not be strings
+    /// <param name="length">The length of the random string to generate.</param>
+    /// <returns>The random bytes encoded as a string that can contain the characters in [a-zA-Z0-9-_], as 
+    /// well as the various symbol characters.</returns>
+    public static string Generate(int length)
     {
-        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(byteCount, 0);
+        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(length, 0);
 
-        var randomStringBytes = ArrayPool<byte>.Shared.Rent(byteCount);
-        try
-        {
-            GenerateRandomBytes(randomStringBytes, byteCount);
+        return RandomNumberGenerator.GetString(_alphanumericPlusSymbols, length);
+    }
 
-            return Base64Url.EncodeToString(randomStringBytes.AsSpan().Slice(0, byteCount));
-        }
-        finally
-        {
-            ArrayPool<byte>.Shared.Return(randomStringBytes, clearArray: true);
-        }
+    /// <summary>
+    /// Generate a cryptographically-strong array of random bytes and return them encoded as a string that
+    /// can contain the characters in [a-zA-Z0-9]. Optionally include dash ('-') and underscore ('_') as possible characters.
+    /// </summary>
+    /// <param name="length">The length of the random string to generate.</param>
+    /// <param name="includeDashAndUnderscore">If true, include both dash ('-') and underscore ('_') as possible characters in the resulting string; otherwise, omit them. Default is false.</param>
+    /// <returns>The random bytes encoded as a string that can contain the characters in [a-zA-Z0-9-_].</returns>
+    public static string GenerateAlphanumeric(int length, bool includeDashAndUnderscore = false)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(length, 0);
+
+        return includeDashAndUnderscore
+            ? RandomNumberGenerator.GetString(_alphanumericPlusDashUnderscore, length)
+            : RandomNumberGenerator.GetString(_alphanumeric, length);
+    }
+
+    /// <summary>
+    /// Generates a cryptographically-strong array of random bytes and return them encoded as a string that
+    /// can contain the characters in [A-Z0-9]. Optionally include dash ('-') and underscore ('_') as possible characters.
+    /// </summary>
+    /// <param name="length">The length of the random string to generate.</param>
+    /// <param name="includeDashAndUnderscore">If true, include both dash ('-') and underscore ('_') as possible characters in the resulting string; otherwise, omit them. Default is false.</param>
+    /// <returns>The random bytes encoded as a string that can contain the characters in [A-Z0-9].</returns>
+    public static string GenerateUppercaseAlphanumeric(int length, bool includeDashAndUnderscore = false)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(length, 0);
+
+        return includeDashAndUnderscore
+            ? RandomNumberGenerator.GetString(_uppercaseAlphanumericPlusDashUnderscore, length)
+            : RandomNumberGenerator.GetString(_uppercaseAlphanumeric, length);
     }
 
     /// <summary>
@@ -94,49 +114,29 @@ public static class RandomString
     }
 
     /// <summary>
-    /// Generates a cryptographically-strong array of random bytes and return them encoded as a string that
-    /// can contain the characters in [A-Z0-9]. Optionally include dash ('-') and underscore ('_') as possible characters.
+    /// Generates a cryptographically-strong array of random bytes and return them as a base64 url-encoded string. 
     /// </summary>
-    /// <param name="length">The length of the random string to generate.</param>
-    /// <param name="includeDashAndUnderscore">If true, include both dash ('-') and underscore ('_') as possible characters in the resulting string; otherwise, omit them. Default is false.</param>
-    /// <returns>The random bytes encoded as a string that can contain the characters in [A-Z0-9].</returns>
-    public static string GenerateUppercaseAlphanumeric(int length, bool includeDashAndUnderscore = false)
+    /// <remarks>The underlying <see cref="Base64Url.EncodeToString(ReadOnlySpan{byte})"/> omits the optional padding characters.</remarks>
+    /// <param name="byteCount">The number of random bytes to generate.</param>
+    /// <returns>The random bytes as a base64 url-encoded string.</returns>
+    // Justification: It's a base64 URL-encoded string, not a URI.
+#pragma warning disable CA1055 // URI-like return values should not be strings
+    public static string GenerateBase64UrlEncoded(int byteCount)
+#pragma warning restore CA1055 // URI-like return values should not be strings
     {
-        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(length, 0);
+        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(byteCount, 0);
 
-        return includeDashAndUnderscore
-            ? RandomNumberGenerator.GetString(_uppercaseAlphanumericPlusDashUnderscore, length)
-            : RandomNumberGenerator.GetString(_uppercaseAlphanumeric, length);
-    }
+        var randomStringBytes = ArrayPool<byte>.Shared.Rent(byteCount);
+        try
+        {
+            GenerateRandomBytes(randomStringBytes, byteCount);
 
-    /// <summary>
-    /// Generate a cryptographically-strong array of random bytes and return them encoded as a string that
-    /// can contain the characters in [a-zA-Z0-9]. Optionally include dash ('-') and underscore ('_') as possible characters.
-    /// </summary>
-    /// <param name="length">The length of the random string to generate.</param>
-    /// <param name="includeDashAndUnderscore">If true, include both dash ('-') and underscore ('_') as possible characters in the resulting string; otherwise, omit them. Default is false.</param>
-    /// <returns>The random bytes encoded as a string that can contain the characters in [a-zA-Z0-9-_].</returns>
-    public static string GenerateAlphanumeric(int length, bool includeDashAndUnderscore = false)
-    {
-        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(length, 0);
-
-        return includeDashAndUnderscore
-            ? RandomNumberGenerator.GetString(_alphanumericPlusDashUnderscore, length)
-            : RandomNumberGenerator.GetString(_alphanumeric, length);
-    }
-
-    /// <summary>
-    /// Generate a cryptographically-strong array of random bytes and return them encoded as a string that
-    /// can contain the characters in [a-zA-Z0-9-_], as well as the various symbol characters.
-    /// </summary>
-    /// <param name="length">The length of the random string to generate.</param>
-    /// <returns>The random bytes encoded as a string that can contain the characters in [a-zA-Z0-9-_], as 
-    /// well as the various symbol characters.</returns>
-    public static string Generate(int length)
-    {
-        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(length, 0);
-
-        return RandomNumberGenerator.GetString(_alphanumericPlusSymbols, length);
+            return Base64Url.EncodeToString(randomStringBytes.AsSpan().Slice(0, byteCount));
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(randomStringBytes, clearArray: true);
+        }
     }
 
 
