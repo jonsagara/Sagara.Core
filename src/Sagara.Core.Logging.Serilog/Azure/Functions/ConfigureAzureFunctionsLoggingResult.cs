4f9ca44c8845b794e80253f9973dc2aa811265a4
Jon Sagara <jonsagara@gmail.com>
2026-04-26 16:06:57 -0700

    Add extension methods to simplify hooking up Serilog to a dotnet-isolated Azure Function.

---
+++ src/Sagara.Core.Logging.Serilog/Azure/Functions/ConfigureAzureFunctionsLoggingResult.cs
@@ -0,0 +1,10 @@
+﻿using Serilog.Extensions.Logging;
+
+namespace Sagara.Core.Logging.Serilog.Azure.Functions;
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="NonSerilogMELProviders"></param>
+public record ConfigureAzureFunctionsLoggingResult(
+    LoggerProviderCollection NonSerilogMELProviders);
